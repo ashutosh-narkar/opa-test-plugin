7ff1c74d779212833774045aa60701da15f85b1f
Ashutosh Narkar <anarkar4387@gmail.com>
2018-04-23 15:15:10 -0700

    Updated OPA version to v0.8.0
    Signed-off-by: Ashutosh Narkar <anarkar4387@gmail.com>

--- build/update-opa-version.sh
+++ build/update-opa-version.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 # Script to revendor OPA. Add and Commit changes if needed
 
+
+usage() {
+    echo "update-opa-version.sh <VERSION> eg. update-opa-version.sh v0.8.0"
+}
+
+# Check if OPA version provided
+if [ $# -eq 0 ]
+  then
+    echo "OPA version not provided"
+	usage
+	exit 1
+fi
+
 # Update the OPA verison in glide.yaml
 sed -i '' "/opa/{N;s/version: .*/version: $1/;}" glide.yaml
 
