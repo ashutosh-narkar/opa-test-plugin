0382848e8cf5ca4db35da51fd102585a65c5271f
Ashutosh Narkar <anarkar4387@gmail.com>
2020-08-14 00:40:07 -0700

    Update the text body
    Signed-off-by: Ashutosh Narkar <anarkar4387@gmail.com>

--- build/github-release.sh
+++ build/github-release.sh
@@ -40,15 +40,16 @@ RELEASE_NOTES="release-notes.md"
 # The hub CLI expects the first line to be the title
 echo -e "${TAG_NAME}\n" > "${RELEASE_NOTES}"
 
-LINE="Some Description"
 # Fill in the description
+LINE="See the OPA ${TAG_NAME} release notes."
 echo -e "${LINE}" >> "${RELEASE_NOTES}"
 
 # Update or create a release on github
 if hub release show "${TAG_NAME}" > /dev/null; then
     # Occurs when the tag is created via GitHub UI w/ a release
     # Use -m "" to preserve the existing text.
-    hub release edit "${ASSETS[@]}" -m "" "${TAG_NAME}"
+    #hub release edit "${ASSETS[@]}" -m "" "${TAG_NAME}"
+    hub release edit "${ASSETS[@]}" -F ${RELEASE_NOTES} "${TAG_NAME}"
 else
     # Create a draft release
     hub release create "${ASSETS[@]}" -F ${RELEASE_NOTES} --draft "${TAG_NAME}"
